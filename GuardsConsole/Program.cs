078782f5c039d5a16f210085997805855e7abac7
Antis <Antis.smith@gmail.com>
2023-04-09 14:30:55 +0300

    add json

---
+++ GuardsConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace GuardsConsole
+{
+    internal class Program
+    {
+        public static void Main(string[] args) { }
+    }
+
+    public class SettingsGuards
+    {
+        public string guard1 = "guard1";
+        public string guard2 = "guard2";
+        public string guard3 = "guard3";
+
+        /// <summary>
+        /// Начальник 
+        /// </summary>
+        public string director = "director";
+
+        /// <summary>
+        /// Заместитель начальника
+        /// </summary>
+        public string deputy_chief = "deputy_chief";
+
+        /// <summary>
+        /// Начальник управления(филиала)
+        /// Branch manager
+        /// </summary>
+        public string supervisor = "supervisor";
+        /// <summary>
+        /// Начальник отдела
+        /// Department head
+        /// </summary>
+        public string superior = "superior";
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class SettingsHolidays
+    {
+        [JsonProperty]
+        public List<DateTime> holidays;
+    }
+}
