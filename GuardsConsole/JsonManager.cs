dfc19eef8d076109079b3362a24121b662dc944b
Antis <Antis.smith@gmail.com>
2023-04-09 18:03:19 +0300

    add чтение выходных праздничных и копирование их в Json

---
+++ GuardsConsole/JsonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+using Newtonsoft.Json;
+
+namespace GuardsConsole
+{
+    public class JsonManager
+    {
+        public static void WriteTest<T>(JsonSerializer serializer, T product)
+        {
+            var path = Environment.CurrentDirectory + @"\json.txt";
+
+            using (var sw = new StreamWriter(path))
+            using (JsonWriter writer = new JsonTextWriter(sw)) { serializer.Serialize(writer, product); }
+        }
+
+        public static void WriteProductTest<T>(T product, string fileName = @"json")
+        {
+            var path = Environment.CurrentDirectory + @"\" + fileName + ".txt";
+
+            var output = JsonConvert.SerializeObject(product, Formatting.Indented, new JsonSerializerSettings
+            {
+                DateFormatHandling = DateFormatHandling.IsoDateFormat
+            });
+
+            using (var sw = new StreamWriter(path)) { sw.Write(output); }
+        }
+    }
+}
--- GuardsConsole/Program.cs
+++ GuardsConsole/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json;
 
 namespace GuardsConsole
 {
     internal class Program
     {
-        public static void Main(string[] args) { }
+        public static void Main(string[] args)
+        {
+            var holidays = HolidaysReader.Read();
+            SettingsHolidays settings = new SettingsHolidays { Holidays = holidays };
+            
+            JsonManager.WriteProductTest(settings);
+        }
+    }
+
+    public static class HolidaysReader
+    {
+        public static List<DateTime> Read()
+        {
+            var holidays = new List<DateTime>();
+            var path = Environment.CurrentDirectory + @"\Holidays.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var result = DateTime.MaxValue;
+                    DateTime.TryParse(line, out result);
+                    if (result != DateTime.MaxValue)
+                    {
+                        holidays.Add(result);
+                    }
+                    
+                    Console.WriteLine(line);
+                }
+            }
+
+            return holidays;
+        }
     }
 
     public class SettingsGuards
@@ -29,18 +62,18 @@ namespace GuardsConsole
         /// Начальник управления(филиала)
         /// Branch manager
         /// </summary>
-        public string supervisor = "supervisor";
+        public string branchManager = "Начальник управления(филиала)";
         /// <summary>
         /// Начальник отдела
         /// Department head
         /// </summary>
-        public string superior = "superior";
+        public string departmentHead = "Начальник отдела";
     }
 
     [JsonObject(MemberSerialization.OptIn)]
     public class SettingsHolidays
     {
         [JsonProperty]
-        public List<DateTime> holidays;
+        public List<DateTime> Holidays { get; set; }
     }
 }
